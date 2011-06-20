a7eb6f3424ed14254235af6ff3011d47888e6321
Deniz KALFA <dkalfa@gmail.com>
2011-06-21 01:29:47 +0300

    Change-Id: Ic93ca26ff3da2553fe23de8c1ead71db0907d0f1

---
+++ src/main/java/model/car/Car.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model.car;
+
+/**
+ * @author Deniz KALFA
+ *
+ */
+public class Car {
+	private final Engine engine;
+
+	public Car(Engine engine) {
+		this.engine = engine;
+	}
+
+	public Engine getEngine() {
+		return engine;
+	}
+}
