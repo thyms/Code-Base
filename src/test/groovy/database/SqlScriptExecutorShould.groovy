64389e04c760b4d6133df9fd6909862473e82d06
Deniz KALFA <dkalfa@gmail.com>
2011-06-25 18:13:18 +0300

    Change-Id: I2e3c9d0e1eed7740e27b576b5802ff723eb61083

--- src/test/groovy/database/SqlScriptExecutorShould.groovy
+++ src/test/groovy/database/SqlScriptExecutorShould.groovy
@@ -13,25 +13,26 @@ import model.database.SqlScriptExecutor
  *
  */
 class SqlScriptExecutorShould extends Specification {
-	@Shared sql = Sql.newInstance("jdbc:h2:mem:test", "sa", "sa", "org.h2.Driver")
+	@Shared sql = Sql.newInstance("jdbc:h2:mem:test", "sa", "", "org.h2.Driver")
 	
 	def "execute sql statements in script file."() {
 		when:
-//		def file = new File("src/main/resources/db.sql")
-//		
-//		Class.forName("org.h2.Driver")
-//		def connection = DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")
-//		def statement = connection.createStatement()
-//		
-//		def sqlScriptExecutor = new SqlScriptExecutor(statement)
-//		sqlScriptExecutor.execute(file)
+		def file = new File("src/main/resources/db.sql")
+		
+		Class.forName("org.h2.Driver")
+		def connection = DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")
+		def statement = connection.createStatement()
+		
+		def sqlScriptExecutor = new SqlScriptExecutor(statement)
+		sqlScriptExecutor.execute(file)
 		sql.execute("CREATE TABLE USERS(FIRST_NAME VARCHAR(255), LAST_NAME VARCHAR(255))")
-		def value = sql.execute("insert into users values ('John', 'Carpenter')")
+		sql.execute("insert into users1 values ('John', 'Carpenter')")
+		def name
+		def surname
+		def row = sql.firstRow("select first_name, last_name from users1")
 		
 		then:
-		arrdf << sql.rows("select first_name, last_name from maxdata")
-//		arrdf[0] = 'John'
-//		first_name == 'John'
-//		last_name == 'Carpenter'
+		row['FIRST_NAME'] == 'John'
+		row.LAST_NAME == 'Carpenter'
 	}
 }
