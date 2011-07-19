f3de373fd1a133a110710f0eacd42074bf85be60
Deniz KALFA <dkalfa@gmail.com>
2011-07-20 02:42:08 +0300

    Change-Id: I9452e6260ceb997e535d1e39bb668129a007affc

--- src/main/java/spring/context/ApplicationConfig.java
+++ src/main/java/spring/context/ApplicationConfig.java
@@ -6,7 +6,6 @@ import model.car.Engine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class ApplicationConfig {
 	@Bean
