cdb105f4a3b509a98b969682fdc3ea44a1be03b3
Deniz KALFA <dkalfa@gmail.com>
2011-09-07 03:52:33 +0300

    ...

--- code.base.main/src/main/java/javam/concurrent/PlaygroundConcurrent.groovy
+++ code.base.main/src/main/java/javam/concurrent/PlaygroundConcurrent.groovy
@@ -1,102 +1,127 @@
-StackClass stack = new StackClass(5);
-new StackPusher("One", stack);
-new StackPusher("Two", stack);
-new StackPopper("Three", stack);
-System.out.println("Main Thread sleeping.");
-try {
-	Thread.sleep(500);
-} catch (InterruptedException e) {
-	e.printStackTrace();
+MyThread t1 = new MyThread(1);
+MyThread t2 = new MyThread(2);
+
+t1.setPriority(Thread.NORM_PRIORITY);
+t2.setPriority(Thread.NORM_PRIORITY);   // won't yield() for lower priority
+t1.start();
+t2.start();
+		
+class MyThread extends Thread {
+	int id;
+	
+	MyThread(int id) {
+		this.id = id;
+	}
+	
+	public synchronized void run(){
+		for(int i=0; i<100; i++) {
+			if( id==1 && i==30 ) {
+					yield();
+			}
+			System.out.println("My id is: " + id);
+		}
+	}
 }
-System.out.println("Exit from Main Thread.");
 
-
-class StackClass {
-	
-		private Object[] stackArray;
-		private volatile int topOfStack;
-		StackClass(int capacity) {
-			stackArray = new Object[capacity];
-			topOfStack = -1;
-		}
-		public synchronized Object pop() {
-			System.out.println(Thread.currentThread() + ": popping");
-			while (isEmpty()) {
-				try {
-					System.out.println(Thread.currentThread()
-							+ ": waiting to pop");
-					wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			Object obj = stackArray[topOfStack];
-			stackArray[topOfStack--] = null;
-			System.out.println(Thread.currentThread()
-					+ ": notifying after pop");
-			notify();
-			return obj;
-		}
-		public synchronized void push(Object element) {
-			System.out.println(Thread.currentThread() + ": pushing");
-			while (isFull()) {
-				try {
-					System.out.println(Thread.currentThread()
-							+ ": waiting to push");
-					wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			stackArray[++topOfStack] = element;
-			System.out.println(Thread.currentThread()
-					+ ": notifying after push");
-			notify();
-		}
-		public boolean isFull() {
-			return topOfStack >= stackArray.length - 1;
-		}
-		public boolean isEmpty() {
-			return topOfStack < 0;
-		}
-	}
-	
-	abstract class StackUser extends Thread {
-	
-		protected StackClass stack;
-		StackUser(String threadName, StackClass stack) {
-			super(threadName);
-			this.stack = stack;
-			System.out.println(this);
-			setDaemon(true);
-			start();
-		}
-	}
-	
-	class StackPopper extends StackUser { // Stack Popper
-	
-		StackPopper(String threadName, StackClass stack) {
-			super(threadName, stack);
-		}
-		public void run() {
-			while (true) {
-				stack.pop();
-			}
-		}
-	}
-	
-	class StackPusher extends StackUser { // Stack Pusher
-	
-		StackPusher(String threadName, StackClass stack) {
-			super(threadName, stack);
-		}
-		public void run() {
-			while (true) {
-				stack.push(new Integer(1));
-			}
-		}
-	}
-	
+//StackClass stack = new StackClass(5);
+//new StackPusher("One", stack);
+//new StackPusher("Two", stack);
+//new StackPopper("Three", stack);
+//System.out.println("Main Thread sleeping.");
+//try {
+//	Thread.sleep(500);
+//} catch (InterruptedException e) {
+//	e.printStackTrace();
+//}
+//System.out.println("Exit from Main Thread.");
+//
+//
+//class StackClass {
+//	
+//		private Object[] stackArray;
+//		private volatile int topOfStack;
+//		StackClass(int capacity) {
+//			stackArray = new Object[capacity];
+//			topOfStack = -1;
+//		}
+//		public synchronized Object pop() {
+//			System.out.println(Thread.currentThread() + ": popping");
+//			while (isEmpty()) {
+//				try {
+//					System.out.println(Thread.currentThread()
+//							+ ": waiting to pop");
+//					wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			Object obj = stackArray[topOfStack];
+//			stackArray[topOfStack--] = null;
+//			System.out.println(Thread.currentThread()
+//					+ ": notifying after pop");
+//			notify();
+//			return obj;
+//		}
+//		public synchronized void push(Object element) {
+//			System.out.println(Thread.currentThread() + ": pushing");
+//			while (isFull()) {
+//				try {
+//					System.out.println(Thread.currentThread()
+//							+ ": waiting to push");
+//					wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			stackArray[++topOfStack] = element;
+//			System.out.println(Thread.currentThread()
+//					+ ": notifying after push");
+//			notify();
+//		}
+//		public boolean isFull() {
+//			return topOfStack >= stackArray.length - 1;
+//		}
+//		public boolean isEmpty() {
+//			return topOfStack < 0;
+//		}
+//	}
+//	
+//	abstract class StackUser extends Thread {
+//	
+//		protected StackClass stack;
+//		StackUser(String threadName, StackClass stack) {
+//			super(threadName);
+//			this.stack = stack;
+//			System.out.println(this);
+//			setDaemon(true);
+//			start();
+//		}
+//	}
+//	
+//	class StackPopper extends StackUser { // Stack Popper
+//	
+//		StackPopper(String threadName, StackClass stack) {
+//			super(threadName, stack);
+//		}
+//		public void run() {
+//			while (true) {
+//				stack.pop();
+//			}
+//		}
+//	}
+//	
+//	class StackPusher extends StackUser { // Stack Pusher
+//	
+//		StackPusher(String threadName, StackClass stack) {
+//			super(threadName, stack);
+//		}
+//		public void run() {
+//			while (true) {
+//				stack.push(new Integer(1));
+//			}
+//		}
+//	}
+//	
 
 //import java.util.concurrent.TimeUnit;
 //
